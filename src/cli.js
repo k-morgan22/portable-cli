70862ff55619dab1bad862ff92c331cdfa3bf9c4
kmorgan <kiki.morgan13@gmail.com>
2020-12-22 02:30:37 -0500

    handling cli list option

--- src/cli.js
+++ src/cli.js
@@ -1,5 +1,41 @@
 import arg from 'arg';
 import inquirer from 'inquirer';
+import os from 'os'
+import fs from 'fs'
+import path from 'path'
+
+
+
+function projectsExist(){
+  const portableDir = path.join(os.homedir(), '/Desktop/vscPortable')
+  console.log(portableDir)
+
+  try {
+    fs.statSync(portableDir).isDirectory();
+    return true
+  } catch (e) {
+    if (e.code === 'ENOENT') {
+      return false;
+    } else {
+      throw e;
+    }
+  }
+
+}
+
+
+function listFolders(){
+  const folders = [];
+  const portableDir = path.join(os.homedir(), '/Desktop/vscPortable')
+
+  const folderNames = fs.readdirSync(portableDir, {withFileTypes: true})
+  for(const name of folderNames){
+    if(name.isDirectory()){
+      folders.push(name.name)
+    }
+  }
+  return folders
+}
 
 function parseArgumentsIntoOptions(rawArgs) {
   const args = arg(
@@ -58,6 +94,29 @@ async function promptForMissingOptions(options) {
   }
 
   if(taskAnswer.task === 'List all existing portable projects'){
+    options.listProjects = true
+  }
+
+  if(options.listProjects){
+
+    try {
+      projectsExist();
+      if(projectsExist){
+        questions.push({
+          type: 'list',
+          name: 'listSelect',
+          message: 'Select an existing project to open',
+          choices: listFolders(),
+        });
+      }
+    } catch (e) {
+      if (e.code === 'ENOENT') {
+        console.log('No portable projects exist. Use portable-cli --create projectName to get started')
+      } else {
+        throw e;
+      }
+    }
+    
   }
 
   const answer = await inquirer.prompt(questions)
@@ -65,7 +124,7 @@ async function promptForMissingOptions(options) {
   return {
     ...options,
     createProject: options.createProject || answer.createName,
-    openProject: options.openProject || answer.openName,
+    openProject: options.openProject || answer.openName || answer.listSelect,
   };
 }
 
