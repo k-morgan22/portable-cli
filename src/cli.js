4fc91b73c003f08596b7d9aad1ad142ad3d706bd
kmorgan <kiki.morgan13@gmail.com>
2020-12-21 20:53:32 -0500

    handling missing options

--- src/cli.js
+++ src/cli.js
@@ -1,4 +1,5 @@
 import arg from 'arg';
+import inquirer from 'inquirer';
 
 function parseArgumentsIntoOptions(rawArgs) {
   const args = arg(
@@ -21,7 +22,55 @@ function parseArgumentsIntoOptions(rawArgs) {
   };
 }
 
-export function cli(args) {
+async function promptForMissingOptions(options) {
+  if(options.listProjects){
+
+  }
+
+  const taskQuestion = [];
+  const questions = []
+  taskQuestion.push({
+    type: 'list',
+    name: 'task',
+    message: 'What would you like to do today?',
+    choices: [
+      'Create a new portable project', 
+      'Open an existing portable project', 
+      'List all existing portable projects'
+    ],
+    // default: 'Create a new portable project'
+  });
+  const taskAnswer = await inquirer.prompt(taskQuestion);
+  if(taskAnswer.task === 'Create a new portable project'){
+    questions.push({
+      type: 'input',
+      name: 'createName',
+      message: 'Project Name?',
+    });
+  }
+
+  if(taskAnswer.task === 'Open an existing portable project'){
+    questions.push({
+      type: 'input',
+      name: 'openName',
+      message: 'Project Name?',
+    });
+  }
+
+  if(taskAnswer.task === 'List all existing portable projects'){
+  }
+
+  const answer = await inquirer.prompt(questions)
+
+  return {
+    ...options,
+    createProject: options.createProject || answer.createName,
+    openProject: options.openProject || answer.openName,
+  };
+}
+
+export async function cli(args) {
   let options = parseArgumentsIntoOptions(args);
+  options = await promptForMissingOptions(options);
   console.log(options);
 }
