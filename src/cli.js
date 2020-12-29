2c2b15c614bf195d3207ab62249a31f06c8c6f07
kmorgan <kiki.morgan13@gmail.com>
2020-12-29 03:57:00 -0500

    move listProjects

--- src/cli.js
+++ src/cli.js
@@ -1,41 +1,9 @@
 import arg from 'arg';
 import inquirer from 'inquirer';
-import os from 'os'
-import fs from 'fs'
-import path from 'path'
 import { createOrOpen } from './main';
+import { projectsExist } from './listHelper';
+import { listFolders } from './listHelper';
 
-
-
-function projectsExist(){
-  const portableDir = path.join(os.homedir(), '/Desktop/vscPortable')
-  
-  try {
-    fs.statSync(portableDir).isDirectory();
-    return true
-  } catch (e) {
-    if (e.code === 'ENOENT') {
-      return false;
-    } else {
-      throw e;
-    }
-  }
-
-}
-
-
-function listFolders(){
-  const folders = [];
-  const portableDir = path.join(os.homedir(), '/Desktop/vscPortable')
-
-  const folderNames = fs.readdirSync(portableDir, {withFileTypes: true})
-  for(const name of folderNames){
-    if(name.isDirectory()){
-      folders.push(name.name)
-    }
-  }
-  return folders
-}
 
 function parseArgumentsIntoOptions(rawArgs) {
   const args = arg(
@@ -100,18 +68,21 @@ async function promptForMissingOptions(options) {
   if(options.listProjects){
 
     try {
-      projectsExist();
+      projectsExist(); 
       if(projectsExist){
-        questions.push({
-          type: 'list',
-          name: 'listSelect',
-          message: 'Select an existing project to open',
-          choices: listFolders(),
-        });
+        const folders = listFolders()
+        if(folders){
+          questions.push({
+            type: 'list',
+            name: 'listSelect',
+            message: 'Select an existing project to open',
+            choices: folders, 
+          });
+        }
       }
     } catch (e) {
       if (e.code === 'ENOENT') {
-        console.log('No portable projects exist. Use portable-cli --create projectName to get started')
+        console.log("No portable projects exist. Use \"portable-cli --create projectName\" to get started")
       } else {
         throw e;
       }
@@ -131,6 +102,5 @@ async function promptForMissingOptions(options) {
 export async function cli(args) {
   let options = parseArgumentsIntoOptions(args);
   options = await promptForMissingOptions(options);
-  // console.log(options);
   await createOrOpen(options)
 }
